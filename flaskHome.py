abddfdeb5678c05aa530698cc7af09243beec795
kow943 <82255591+kow943@users.noreply.github.com>
2021-05-28 17:02:43 +0800

    changes
    base.html now only have navs
    index.html have the tabcontents

--- flaskHome.py
+++ flaskHome.py
@@ -1,5 +1,5 @@
 from flask import Flask, redirect, url_for, render_template, request, session
-from datetime import datetime, timedelta
+from datetime import datetime
 
 app = Flask(__name__, template_folder= 'templates')
 
@@ -13,50 +13,8 @@ def base():
         'date': date[0],
         'time': date[1]
     }
-    return render_template('base.html', info = info)
+    return render_template('index.html', info = info)
 
-'''
-@app.route("/", methods=["POST", "GET"])
-def homePage():
-    if request.method == "POST":
-        location = request.form["location"]
-        dt = str(datetime.now().isoformat(timespec='seconds', sep=' '))
-        return render_template("current.html", location = location, dt = dt)
-    else:
-        return render_template("current.html")
-
-@app.route('/hourly', methods=["POST", "GET"])
-def hourly():
-    if request.method == "POST":
-        location = request.form["location"]
-        dt = str(datetime.now().isoformat(timespec='seconds', sep=' '))
-        return render_template("hourly.html", location = location, dt = dt)
-    else:
-        return render_template('hourly.html')
-
-@app.route('/weekly', methods=["POST", "GET"])
-def weekly():
-    if request.method == "POST":
-        location = request.form["location"]
-        dt = str(datetime.now().isoformat(timespec='seconds', sep=' '))
-        return render_template("weekly.html", location = location, dt = dt)
-    else:
-        return render_template('weekly.html')
-
-@app.route("/search", methods=["POST", "GET"])
-def search():
-    searchedString = request.args.get("search") # TODO: Matched against DB
-    return render_template("search.html")
-
-##@app.route("/location", methods=["POST", "GET"])
-##def location():
-##    if request.method == "POST":
-##        location = request.form["location"]
-##        dt = str(datetime.now().isoformat(timespec='seconds', sep=' '))
-##        return render_template("location.html", location = location, dt = dt)
-##    else:
-##        return render_template("location.html")
-'''
 if __name__ == "__main__":
     app.run(host="localhost", debug = True) # Simply refresh the opened webpage,
                             # but Python Shell requires manual restart to quit
