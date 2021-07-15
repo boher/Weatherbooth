5c51813a7128134a4371fc4adfff85b45ef44fbf
agarwal042 <62708505+agarwal042@users.noreply.github.com>
2021-07-15 19:23:39 +0800

    converted output from classes into dictionaries

--- website/weatherDisplay.py
+++ website/weatherDisplay.py
@@ -9,11 +9,46 @@ weatherDisplay = Blueprint('weatherDisplay', __name__)
 @weatherDisplay.route("/")
 def currentPage():
 
-    current = CurrentHourWeather()
+    c = CurrentHourWeather()
+    current ={
+        'temp': c.temp,
+        'tm': c.tm,
+        'hm': c.hm,
+        'cm': c.cm,
+        'wsm': c.wsm,
+        'pm': c.pm,
+        'cond': c.cond,
+        'date': c.date,
+        'time': c.time,
+        'uvi': c.uvi,
+        'humd': c.humd,
+        'cloud': c.cloud,
+        'ws': c.ws,
+        'p': c.p,
+        'img': c.img,
+        'rain': c.rain,
+    }
 
-    tfHour = TwentyFourHourWeather()
+    tf = TwentyFourHourWeather()
+    tfHour = {
+        't': tf.t,
+        'h': tf.h,
+        'pe': tf.pe,
+        'a': tf.a,
+        "w": tf.w,
+        "c": tf.c
+    }
 
-    dataframe = tfHour.getDataFrame()
-    sDay = SevenDayWeather(dataframe, tfHour)
+    dataframe = tf.getDataFrame()
+    s = SevenDayWeather(dataframe, tf)
+    res ={
+        'test1':s.test1,
+        'test2':s.test2,
+        'test3':s.test3,
+        'test4':s.test4,
+        'test5':s.test5,
+        'test6':s.test6,
+        'test7':s.test7
+    }
 
-    return render_template('index.html', current = current, tfHour = tfHour, res = sDay)
+    return render_template('index.html', current = current, tfHour = tfHour, res = res)
