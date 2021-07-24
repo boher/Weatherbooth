4b9b686f110947675a10e789364cd7b338a0684e
gerlau <gerlaujle@gmail.com>
2021-07-24 15:27:38 +0800

    Testing deployment

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Deploy from './Component/Deploy/Deploy';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Deploy />
+    </div>
+  );
+}
+
+export default App;
