10e677e00bd0ff36a3b0cd46a702940821febea7
agarwal042 <62708505+agarwal042@users.noreply.github.com>
2021-08-04 14:23:31 +0800

    javascript changes

--- website/static/main.js
+++ website/static/main.js
@@ -132,7 +132,4 @@ var wsV = document.querySelectorAll("#wsValue");
     // document.getElementById('displayMsg').innerHTML = "Thank you for your feedback!! Please wait for the website to re-load.";
     // document.getElementById('displayMsg').style.fontWeight = 'bold';
     alert("Thank you for your feedback!!!");
-    closeForm();
-
-
   }
